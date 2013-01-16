0fe0df55281fa961ffde43497cc47b5ab74ec575
murrayju <murrayju@addpcs.com>
2013-01-16 14:10:20 -0500

    Fix for windows bootstrap file copy

--- winbootstrap.bat
+++ winbootstrap.bat
@@ -4,5 +4,5 @@ echo Gitting latest
 call git pull
 
 echo Copying Files
-xcopy .vim %HOME% /F /E /I /H /R /Y /B /EXCLUDE:.xcopyignore
+xcopy .vim %HOME%\.vim /F /E /I /H /R /Y /B /EXCLUDE:.xcopyignore
 xcopy .vimrc %HOME% /F /E /I /H /R /Y /B /EXCLUDE:.xcopyignore
