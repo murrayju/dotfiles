30be09e6df2d2464d79bec22e5b8344ebb7e3d85
murrayju <murrayju@addpcs.com>
2013-01-17 17:40:15 -0500

    Updated to detect chocolatey already installed

--- winChocolateyApps.bat
+++ winChocolateyApps.bat
@@ -2,43 +2,47 @@
 
 rem This installs Chocolatey NuGet, and a set of basic apps
 
-rem Install Chocolatey first
+rem Update Chocolatey if it is installed
+call cup
+if %errorlevel%==0 goto noInst
+rem Install Chocolatey if it is missing
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('http://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+:noInst
 
 rem basic apps
-cinst 7zip
-cinst 7zip.commandline
-cinst freecommander
+call cinst 7zip
+call cinst 7zip.commandline
+call cinst freecommander
 
 rem browsers
-cinst GoogleChrome
-cinst Firefox
+call cinst GoogleChrome
+call cinst Firefox
 
 rem Install GnuWin32 apps
-cinst GnuWin
+call cinst GnuWin
 
 rem Sysinternals package
-cinst sysinternals
+call cinst sysinternals
 
 rem Dev tools
-cinst git
-cinst tortoisegit
-cinst vim
-cinst notepadplusplus
-cinst notepad2
-cinst python
-cinst dotPeek
-cinst DotNet4.5
-cinst wireshark
+call cinst git
+call cinst tortoisegit
+call cinst vim
+call cinst notepadplusplus
+call cinst notepad2
+call cinst python
+call cinst dotPeek
+call cinst DotNet4.5
+call cinst wireshark
 
 rem misc apps
-cinst filezilla
-cinst launchy
-cinst fiddler
-cinst autohotkey_l
+call cinst filezilla
+call cinst launchy
+call cinst fiddler
+call cinst autohotkey_l
 cisnt putty
-cinst VirtualCloneDrive
-cinst vlc
-cinst windirstat
-cinst winscp
-cinst javaruntime
+call cinst VirtualCloneDrive
+call cinst vlc
+call cinst windirstat
+call cinst winscp
+call cinst javaruntime
