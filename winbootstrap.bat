5fe0aa3b30a6a4513119c6d2fc3dddfb0708fbe8
Justin Murray <murrayju@addpcs.com>
2013-04-16 15:12:26 -0400

    Added vim-lawrencium (mercurial plugin)

--- winbootstrap.bat
+++ winbootstrap.bat
@@ -14,3 +14,6 @@ set vimexe="%ProgramFiles%\vim\vim73\vim.exe"
 if exist %vimexe% (
 	call %vimexe% -u .vimrc.bundles +BundleInstall! +BundleClean +qall
 )
+
+:: Non-vundle bundles
+hg clone https://bitbucket.org/ludovicchabant/vim-lawrencium "%HOME%\.vim\bundle\vim-lawrencium"
