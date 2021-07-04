e66f5c3f72a324f5b09c9ae91eeb67f7b45a98b5
Justin Murray <justin@murrayju.com>
2021-07-04 13:59:33 -0600

    improve bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,33 +1,35 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+root="$(dirname ${BASH_SOURCE})"
+cd "${root}";
 
 git pull origin master;
 
 function doIt() {
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	rsync --exclude ".git/" \
-		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
-		--exclude "README.md" \
-		--exclude "win/" \
-		--exclude "*.bat" \
-		--exclude "LICENSE-MIT.txt" \
-		-avh --no-perms . ~;
-	source ~/.bash_profile;
-	vim -c "PlugInstall" +qall
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  rsync --exclude ".git/" \
+    --exclude ".DS_Store" \
+    --exclude ".osx" \
+    --exclude "bootstrap.sh" \
+    --exclude "README.md" \
+    --exclude "win/" \
+    --exclude "*.bat" \
+    --exclude "LICENSE-MIT.txt" \
+    -avh --no-perms . ~;
+  ln -sf "${root}/.vimrc" ~/.vimrc
+  source ~/.bash_profile;
+  vim -c "PlugInstall" +qall
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+  doIt;
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+  echo "";
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt;
+  fi;
 fi;
 unset doIt;
